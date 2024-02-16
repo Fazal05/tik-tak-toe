69ce6a63ba2deab6d831c8ede825200ee02b0d8d
Fazal05 <158821687+Fazal05@users.noreply.github.com>
2024-02-16 22:58:48 +0530

    Update tik.js

--- tik.js
+++ tik.js
@@ -9,7 +9,7 @@ const h=document.getElementById("h");
 const i=document.getElementById("i");
 const text=document.getElementById("text");
 const btn=document.querySelector("button");
-let myarr=["red","blue","red","blue","red","blue","red","blue","red"];
+let myarr=["red","blue","red","blue","red","blue","red","blue","red","blue","red","blue","red","blue"];
 let j=0;
 let af=0;
 let bf=0;
@@ -20,148 +20,364 @@ let ff=0;
 let gf=0;
 let hf=0;
 let iff=0;
+let ab=0;
+let bb=0;
+let cb=0;
+let db=0;
+let eb=0;
+let fb=0;
+let gb=0;
+let hb=0;
+let ib=0;
+let red=0;
+let blue=0;
+let blue1=0;
+let red1=0;
+let draw=0;
+let draw1=0;
+const rs=document.getElementById("rs");
+const bs=document.getElementById("bs");
+const ds=document.getElementById("ds");
+const next=document.getElementById("next");
 
 
 
-if(af==1&bf==1&cf==1){
-    text.innerHTML="over";
-    }
 
 
 a.addEventListener("click", non=()=>{
-if(j%2==0){
-    af=2;
-}
-else{
-    af=1;
-}
-   a.style.backgroundColor=myarr[j++]; 
-   if((af==2&bf==2&cf==2)|(af==2&df==2&gf==2)|(gf==2&hf==2&iff==2)|(cf==2&ff==2&iff==2)|(af==2&ef==2&iff==2)|(gf==2&ef==2&cf==2)|(df==2&ef==2&ff==2)|(bf==2&ef==2&hf==2)){
-    text.innerHTML="Gameover- Red won";
+
+if(ab==0){
+    if(j%2==0){
+        af=2;
     }
-    if((af==1&bf==1&cf==1)|(af==1&df==1&gf==1)|(gf==1&hf==1&iff==1)|(cf==1&ff==1&iff==1)|(af==1&ef==1&iff==1)|(gf==1&ef==1&cf==1)|(df==1&ef==1&ff==1)|(bf==1&ef==1&hf==1)){
-        text.innerHTML="Gameover- Blue won";
+    else{
+        af=1;
+    }
+    
+        a.style.backgroundColor=myarr[j++]; 
+    
+    
+    
+    
+      
+       if(((af==2&bf==2&cf==2)|(af==2&df==2&gf==2)|(gf==2&hf==2&iff==2)|(cf==2&ff==2&iff==2)|(af==2&ef==2&iff==2)|(gf==2&ef==2&cf==2)|(df==2&ef==2&ff==2)|(bf==2&ef==2&hf==2))&(blue==0)&(draw==0)){
+        text.innerHTML="Gameover- Red won";
+        red++;
+        red1++;
+        rs.innerHTML=red1;
         }
+        if(((af==1&bf==1&cf==1)|(af==1&df==1&gf==1)|(gf==1&hf==1&iff==1)|(cf==1&ff==1&iff==1)|(af==1&ef==1&iff==1)|(gf==1&ef==1&cf==1)|(df==1&ef==1&ff==1)|(bf==1&ef==1&hf==1))&(red==0)&(draw==0)){
+            text.innerHTML="Gameover- Blue won";
+            blue++;
+            blue1++;
+            bs.innerHTML=blue1;
+            }
+    
+            if(af>0&bf>0&cf>0&df>0&ef>0&ff>0&gf>0&hf>0&iff>0&red==0&blue==0){
+                text.innerHTML="Gameover -Draw Match";
+            draw++;
+            draw1++;
+            ds.innerHTML=draw1;
+            }
+            ab++;   
+
+
+}
 
 
   });
 b.addEventListener("click", non=()=>{
-    if(j%2==0){
-        bf=2;
-    }
-    else{
-        bf=1;
-    }
-      b.style.backgroundColor=myarr[j++];
-      if((af==2&bf==2&cf==2)|(af==2&df==2&gf==2)|(gf==2&hf==2&iff==2)|(cf==2&ff==2&iff==2)|(af==2&ef==2&iff==2)|(gf==2&ef==2&cf==2)|(df==2&ef==2&ff==2)|(bf==2&ef==2&hf==2)){
-        text.innerHTML="Gameover- Red won";
+    if(bb==0){
+
+        if(j%2==0){
+            bf=2;
         }
-        if((af==1&bf==1&cf==1)|(af==1&df==1&gf==1)|(gf==1&hf==1&iff==1)|(cf==1&ff==1&iff==1)|(af==1&ef==1&iff==1)|(gf==1&ef==1&cf==1)|(df==1&ef==1&ff==1)|(bf==1&ef==1&hf==1)){
-            text.innerHTML="Gameover- Blue won";
+        else{
+            bf=1;
+        }
+          b.style.backgroundColor=myarr[j++];
+          if(((af==2&bf==2&cf==2)|(af==2&df==2&gf==2)|(gf==2&hf==2&iff==2)|(cf==2&ff==2&iff==2)|(af==2&ef==2&iff==2)|(gf==2&ef==2&cf==2)|(df==2&ef==2&ff==2)|(bf==2&ef==2&hf==2))&(blue==0)&(draw==0)){
+            text.innerHTML="Gameover- Red won";
+            red++;
+            red1++;
+            rs.innerHTML=red1;
             }
+            if(((af==1&bf==1&cf==1)|(af==1&df==1&gf==1)|(gf==1&hf==1&iff==1)|(cf==1&ff==1&iff==1)|(af==1&ef==1&iff==1)|(gf==1&ef==1&cf==1)|(df==1&ef==1&ff==1)|(bf==1&ef==1&hf==1))&(red==0)&(draw==0)){
+                text.innerHTML="Gameover- Blue won";
+                blue++;
+                blue1++;
+                bs.innerHTML=blue1;
+
+                }
+                
+    if(af>0&bf>0&cf>0&df>0&ef>0&ff>0&gf>0&hf>0&iff>0&red==0&blue==0){
+        text.innerHTML="Gameover -Draw Match";
+        draw++;
+        draw1++;
+        ds.innerHTML=draw1;
+    }
+    bb++;
+    }
     });
 c.addEventListener("click", non=()=>{
-    if(j%2==0){
-        cf=2;
-    }
-    else{
-        cf=1;
-    }
-    c.style.backgroundColor=myarr[j++];
-    if((af==2&bf==2&cf==2)|(af==2&df==2&gf==2)|(gf==2&hf==2&iff==2)|(cf==2&ff==2&iff==2)|(af==2&ef==2&iff==2)|(gf==2&ef==2&cf==2)|(df==2&ef==2&ff==2)|(bf==2&ef==2&hf==2)){
-        text.innerHTML="Gameover- Red won";
+    if(cb==0){
+
+        if(j%2==0){
+            cf=2;
         }
-        if((af==1&bf==1&cf==1)|(af==1&df==1&gf==1)|(gf==1&hf==1&iff==1)|(cf==1&ff==1&iff==1)|(af==1&ef==1&iff==1)|(gf==1&ef==1&cf==1)|(df==1&ef==1&ff==1)|(bf==1&ef==1&hf==1)){
-            text.innerHTML="Gameover- Blue won";
+        else{
+            cf=1;
+        }
+        c.style.backgroundColor=myarr[j++];
+        if(((af==2&bf==2&cf==2)|(af==2&df==2&gf==2)|(gf==2&hf==2&iff==2)|(cf==2&ff==2&iff==2)|(af==2&ef==2&iff==2)|(gf==2&ef==2&cf==2)|(df==2&ef==2&ff==2)|(bf==2&ef==2&hf==2))&(blue==0)&(draw==0)){
+            text.innerHTML="Gameover- Red won";
+            red++;
+            red1++;
+            rs.innerHTML=red1;
             }
+            if(((af==1&bf==1&cf==1)|(af==1&df==1&gf==1)|(gf==1&hf==1&iff==1)|(cf==1&ff==1&iff==1)|(af==1&ef==1&iff==1)|(gf==1&ef==1&cf==1)|(df==1&ef==1&ff==1)|(bf==1&ef==1&hf==1))&(red==0)&(draw==0)){
+                text.innerHTML="Gameover- Blue won";
+                blue++;
+                blue1++;
+                bs.innerHTML=blue1;
+                }
+                
+    if(af>0&bf>0&cf>0&df>0&ef>0&ff>0&gf>0&hf>0&iff>0&red==0&blue==0){
+        text.innerHTML="Gameover -Draw Match";
+        draw++;
+        draw1++;
+        ds.innerHTML=draw1;
+    }
+    cb++;
+    }
   });
 d.addEventListener("click", non=()=>{
-    if(j%2==0){
-        df=2;
-    }
-    else{
-        df=1;
-    }
-    d.style.backgroundColor=myarr[j++];
-    if((af==2&bf==2&cf==2)|(af==2&df==2&gf==2)|(gf==2&hf==2&iff==2)|(cf==2&ff==2&iff==2)|(af==2&ef==2&iff==2)|(gf==2&ef==2&cf==2)|(df==2&ef==2&ff==2)|(bf==2&ef==2&hf==2)){
-        text.innerHTML="Gameover- Red won";
+    if(db==0){
+
+        if(j%2==0){
+            df=2;
         }
-        if((af==1&bf==1&cf==1)|(af==1&df==1&gf==1)|(gf==1&hf==1&iff==1)|(cf==1&ff==1&iff==1)|(af==1&ef==1&iff==1)|(gf==1&ef==1&cf==1)|(df==1&ef==1&ff==1)|(bf==1&ef==1&hf==1)){
-            text.innerHTML="Gameover- Blue won";
+        else{
+            df=1;
+        }
+        d.style.backgroundColor=myarr[j++];
+        if(((af==2&bf==2&cf==2)|(af==2&df==2&gf==2)|(gf==2&hf==2&iff==2)|(cf==2&ff==2&iff==2)|(af==2&ef==2&iff==2)|(gf==2&ef==2&cf==2)|(df==2&ef==2&ff==2)|(bf==2&ef==2&hf==2))&(blue==0)&(draw==0)){
+            text.innerHTML="Gameover- Red won";
+            red++;
+            red1++;
+            rs.innerHTML=red1;
             }
+            if(((af==1&bf==1&cf==1)|(af==1&df==1&gf==1)|(gf==1&hf==1&iff==1)|(cf==1&ff==1&iff==1)|(af==1&ef==1&iff==1)|(gf==1&ef==1&cf==1)|(df==1&ef==1&ff==1)|(bf==1&ef==1&hf==1))&(red==0)&(draw==0)){
+                text.innerHTML="Gameover- Blue won";
+                blue++;
+                blue1++;
+                bs.innerHTML=blue1;
+                }
+                
+    if(af>0&bf>0&cf>0&df>0&ef>0&ff>0&gf>0&hf>0&iff>0&red==0&blue==0){
+        text.innerHTML="Gameover -Draw Match";
+        draw++;
+        draw1++;
+        ds.innerHTML=draw1;
+    }
+    db++;
+    }
   });
   e.addEventListener("click", non=()=>{
-    if(j%2==0){
-        ef=2;
-    }
-    else{
-        ef=1;
-    }
-      e.style.backgroundColor=myarr[j++];
-      if((af==2&bf==2&cf==2)|(af==2&df==2&gf==2)|(gf==2&hf==2&iff==2)|(cf==2&ff==2&iff==2)|(af==2&ef==2&iff==2)|(gf==2&ef==2&cf==2)|(df==2&ef==2&ff==2)|(bf==2&ef==2&hf==2)){
-        text.innerHTML="Gameover- Red won";
+    if(eb==0){
+
+        if(j%2==0){
+            ef=2;
         }
-        if((af==1&bf==1&cf==1)|(af==1&df==1&gf==1)|(gf==1&hf==1&iff==1)|(cf==1&ff==1&iff==1)|(af==1&ef==1&iff==1)|(gf==1&ef==1&cf==1)|(df==1&ef==1&ff==1)|(bf==1&ef==1&hf==1)){
-            text.innerHTML="Gameover- Blue won";
+        else{
+            ef=1;
+        }
+          e.style.backgroundColor=myarr[j++];
+          if(((af==2&bf==2&cf==2)|(af==2&df==2&gf==2)|(gf==2&hf==2&iff==2)|(cf==2&ff==2&iff==2)|(af==2&ef==2&iff==2)|(gf==2&ef==2&cf==2)|(df==2&ef==2&ff==2)|(bf==2&ef==2&hf==2))&(blue==0)&(draw==0)){
+            text.innerHTML="Gameover- Red won";
+            red++;
+            red1++;
+            rs.innerHTML=red1;
             }
+            if(((af==1&bf==1&cf==1)|(af==1&df==1&gf==1)|(gf==1&hf==1&iff==1)|(cf==1&ff==1&iff==1)|(af==1&ef==1&iff==1)|(gf==1&ef==1&cf==1)|(df==1&ef==1&ff==1)|(bf==1&ef==1&hf==1))&(red==0)&(draw==0)){
+                text.innerHTML="Gameover- Blue won";
+                blue++;
+                blue1++;
+                bs.innerHTML=blue1;
+                }
+                
+    if(af>0&bf>0&cf>0&df>0&ef>0&ff>0&gf>0&hf>0&iff>0&red==0&blue==0){
+        text.innerHTML="Gameover -Draw Match";
+        draw++;
+        draw1++;
+        ds.innerHTML=draw1;
+    }
+    eb++;
+    }
     });
     f.addEventListener("click", non=()=>{
-        if(j%2==0){
-            ff=2;
-        }
-        else{
-            ff=1;
-        }
-        f.style.backgroundColor=myarr[j++];
-        if((af==2&bf==2&cf==2)|(af==2&df==2&gf==2)|(gf==2&hf==2&iff==2)|(cf==2&ff==2&iff==2)|(af==2&ef==2&iff==2)|(gf==2&ef==2&cf==2)|(df==2&ef==2&ff==2)|(bf==2&ef==2&hf==2)){
-            text.innerHTML="Gameover- Red won";
+        if(fb==0){
+
+            if(j%2==0){
+                ff=2;
             }
-            if((af==1&bf==1&cf==1)|(af==1&df==1&gf==1)|(gf==1&hf==1&iff==1)|(cf==1&ff==1&iff==1)|(af==1&ef==1&iff==1)|(gf==1&ef==1&cf==1)|(df==1&ef==1&ff==1)|(bf==1&ef==1&hf==1)){
-                text.innerHTML="Gameover- Blue won";
+            else{
+                ff=1;
+            }
+            f.style.backgroundColor=myarr[j++];
+            if(((af==2&bf==2&cf==2)|(af==2&df==2&gf==2)|(gf==2&hf==2&iff==2)|(cf==2&ff==2&iff==2)|(af==2&ef==2&iff==2)|(gf==2&ef==2&cf==2)|(df==2&ef==2&ff==2)|(bf==2&ef==2&hf==2))&(blue==0)&(draw==0)){
+                text.innerHTML="Gameover- Red won";
+                red++;
+                red1++;
+                rs.innerHTML=red1;
                 }
+                if(((af==1&bf==1&cf==1)|(af==1&df==1&gf==1)|(gf==1&hf==1&iff==1)|(cf==1&ff==1&iff==1)|(af==1&ef==1&iff==1)|(gf==1&ef==1&cf==1)|(df==1&ef==1&ff==1)|(bf==1&ef==1&hf==1))&(red==0)&(draw==0)){
+                    text.innerHTML="Gameover- Blue won";
+                    blue++;
+                    blue1++;
+                    bs.innerHTML=blue1;
+                    }
+                    
+    if(af>0&bf>0&cf>0&df>0&ef>0&ff>0&gf>0&hf>0&iff>0&red==0&blue==0){
+        text.innerHTML="Gameover -Draw Match";
+        draw++;
+    }
+    fb++;
+        }
     });
     g.addEventListener("click", non=()=>{
-        if(j%2==0){
-            gf=2;
-        }
-        else{
-            gf=1;
-        }
-        g.style.backgroundColor=myarr[j++];
-        if((af==2&bf==2&cf==2)|(af==2&df==2&gf==2)|(gf==2&hf==2&iff==2)|(cf==2&ff==2&iff==2)|(af==2&ef==2&iff==2)|(gf==2&ef==2&cf==2)|(df==2&ef==2&ff==2)|(bf==2&ef==2&hf==2)){
-            text.innerHTML="Gameover- Red won";
+        if(gb==0){
+
+            if(j%2==0){
+                gf=2;
             }
-            if((af==1&bf==1&cf==1)|(af==1&df==1&gf==1)|(gf==1&hf==1&iff==1)|(cf==1&ff==1&iff==1)|(af==1&ef==1&iff==1)|(gf==1&ef==1&cf==1)|(df==1&ef==1&ff==1)|(bf==1&ef==1&hf==1)){
-                text.innerHTML="Gameover- Blue won";
+            else{
+                gf=1;
+            }
+            g.style.backgroundColor=myarr[j++];
+            if(((af==2&bf==2&cf==2)|(af==2&df==2&gf==2)|(gf==2&hf==2&iff==2)|(cf==2&ff==2&iff==2)|(af==2&ef==2&iff==2)|(gf==2&ef==2&cf==2)|(df==2&ef==2&ff==2)|(bf==2&ef==2&hf==2))&(blue==0)&(draw==0)){
+                text.innerHTML="Gameover- Red won";
+                red++;
+                red1++;
+                rs.innerHTML=red1;
                 }
+                if(((af==1&bf==1&cf==1)|(af==1&df==1&gf==1)|(gf==1&hf==1&iff==1)|(cf==1&ff==1&iff==1)|(af==1&ef==1&iff==1)|(gf==1&ef==1&cf==1)|(df==1&ef==1&ff==1)|(bf==1&ef==1&hf==1))&(red==0)&(draw==0)){
+                    text.innerHTML="Gameover- Blue won";
+                    blue++;
+                    blue1++;
+                    bs.innerHTML=blue1;
+                    }
+                    
+    if(af>0&bf>0&cf>0&df>0&ef>0&ff>0&gf>0&hf>0&iff>0&red==0&blue==0){
+        text.innerHTML="Gameover -Draw Match";
+        draw++;
+        draw1++;
+        ds.innerHTML=draw1;
+    }
+    gb++;
+        }
     });
     h.addEventListener("click", non=()=>{
-        if(j%2==0){
-            hf=2;
-        }
-        else{
-            hf=1;
-        }
-      h.style.backgroundColor=myarr[j++];
-      if((af==2&bf==2&cf==2)|(af==2&df==2&gf==2)|(gf==2&hf==2&iff==2)|(cf==2&ff==2&iff==2)|(af==2&ef==2&iff==2)|(gf==2&ef==2&cf==2)|(df==2&ef==2&ff==2)|(bf==2&ef==2&hf==2)){
-        text.innerHTML="Gameover- Red won";
-        }
-        if((af==1&bf==1&cf==1)|(af==1&df==1&gf==1)|(gf==1&hf==1&iff==1)|(cf==1&ff==1&iff==1)|(af==1&ef==1&iff==1)|(gf==1&ef==1&cf==1)|(df==1&ef==1&ff==1)|(bf==1&ef==1&hf==1)){
-            text.innerHTML="Gameover- Blue won";
+        if(hb==0){
+
+            if(j%2==0){
+                hf=2;
             }
+            else{
+                hf=1;
+            }
+          h.style.backgroundColor=myarr[j++];
+          if(((af==2&bf==2&cf==2)|(af==2&df==2&gf==2)|(gf==2&hf==2&iff==2)|(cf==2&ff==2&iff==2)|(af==2&ef==2&iff==2)|(gf==2&ef==2&cf==2)|(df==2&ef==2&ff==2)|(bf==2&ef==2&hf==2))&(blue==0)&(draw==0)){
+            text.innerHTML="Gameover- Red won";
+            red++;
+            red1++;
+            rs.innerHTML=red1;
+            }
+            if(((af==1&bf==1&cf==1)|(af==1&df==1&gf==1)|(gf==1&hf==1&iff==1)|(cf==1&ff==1&iff==1)|(af==1&ef==1&iff==1)|(gf==1&ef==1&cf==1)|(df==1&ef==1&ff==1)|(bf==1&ef==1&hf==1))&(red==0)&(draw==0)){
+                text.innerHTML="Gameover- Blue won";
+                blue++;
+                blue1++;
+                bs.innerHTML=blue1;
+                }
+                
+    if(af>0&bf>0&cf>0&df>0&ef>0&ff>0&gf>0&hf>0&iff>0&red==0&blue==0){
+        text.innerHTML="Gameover -Draw Match";
+        draw++;
+        draw1++;
+        ds.innerHTML=draw1;
+    }
+    hb++;
+        }
     });
     i.addEventListener("click", non=()=>{
-        if(j%2==0){
-            iff=2;
-        }
-        else{
-            iff=1;
-        }
-        i.style.backgroundColor=myarr[j++];
-        if((af==2&bf==2&cf==2)|(af==2&df==2&gf==2)|(gf==2&hf==2&iff==2)|(cf==2&ff==2&iff==2)|(af==2&ef==2&iff==2)|(gf==2&ef==2&cf==2)|(df==2&ef==2&ff==2)|(bf==2&ef==2&hf==2)){
-            text.innerHTML="Gameover- Red won";
+        if(ib==0){
+
+            if(j%2==0){
+                iff=2;
             }
-            if((af==1&bf==1&cf==1)|(af==1&df==1&gf==1)|(gf==1&hf==1&iff==1)|(cf==1&ff==1&iff==1)|(af==1&ef==1&iff==1)|(gf==1&ef==1&cf==1)|(df==1&ef==1&ff==1)|(bf==1&ef==1&hf==1)){
-                text.innerHTML="Gameover- Blue won";
+            else{
+                iff=1;
+            }
+            i.style.backgroundColor=myarr[j++];
+            if(((af==2&bf==2&cf==2)|(af==2&df==2&gf==2)|(gf==2&hf==2&iff==2)|(cf==2&ff==2&iff==2)|(af==2&ef==2&iff==2)|(gf==2&ef==2&cf==2)|(df==2&ef==2&ff==2)|(bf==2&ef==2&hf==2))&(blue==0)&(draw==0)){
+                text.innerHTML="Gameover- Red won";
+                red++;
+                red1++;
+                rs.innerHTML=red1;
                 }
+                if(((af==1&bf==1&cf==1)|(af==1&df==1&gf==1)|(gf==1&hf==1&iff==1)|(cf==1&ff==1&iff==1)|(af==1&ef==1&iff==1)|(gf==1&ef==1&cf==1)|(df==1&ef==1&ff==1)|(bf==1&ef==1&hf==1))&(red==0)&(draw==0)){
+                    text.innerHTML="Gameover- Blue won";
+                    blue++;
+                    blue1++;
+                    bs.innerHTML=blue1;
+                    }
+                    
+    if(af>0&bf>0&cf>0&df>0&ef>0&ff>0&gf>0&hf>0&iff>0&red==0&blue==0){
+        text.innerHTML="Gameover -Draw Match";
+        draw++;
+        draw1++;
+        ds.innerHTML=draw1;
+    }
+    ib++;
+        }
       });
+
+
+
+      next.addEventListener("click",nxt=()=>{
+
+        a.style.backgroundColor="rgb(250, 246, 246)";
+         b.style.backgroundColor="rgb(250, 246, 246)";
+         c.style.backgroundColor="rgb(250, 246, 246)";
+         d.style.backgroundColor="rgb(250, 246, 246)";
+         e.style.backgroundColor="rgb(250, 246, 246)";
+         f.style.backgroundColor="rgb(250, 246, 246)";
+         g.style.backgroundColor="rgb(250, 246, 246)";
+         h.style.backgroundColor="rgb(250, 246, 246)"; 
+         i.style.backgroundColor="rgb(250, 246, 246)";
+         text.innerHTML="Start";
+         j=0;
+         ab=0;
+         bb=0;
+         cb=0;
+         db=0;
+         eb=0;
+         fb=0;
+         gb=0;
+         hb=0;
+         ib=0;
+         af=0;
+         bf=0;
+         cf=0;
+         df=0;
+         ef=0;
+         ff=0;
+         gf=0;
+         hf=0;
+         iff=0;
+         blue=0;
+         red=0;
+         draw=0;
+        
+     
+     })
